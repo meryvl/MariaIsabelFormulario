5a203bc5edfa743d37a13c8ce85a44577f7902d7
meryvl <meryvalerolaguna@gmail.com>
2023-02-21 11:00:20 +0000

    commit alert

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+  const cvc = document.querySelector('#cvc').value;
+  const alert = document.querySelector('.alert');
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (forms == forms.length == 0 ) {
+          event.preventDefault()
+          event.stopPropagation()
+          alert.innerHTML = ` <div class="alert alert-danger  fw-bold alert-none-valid" role="alert">
+          Some field are missing
+        </div>`
+        }
+        
+        
+        form.classList.add('was-validated')
+      }, false)
+    })
+})();
 
