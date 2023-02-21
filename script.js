5217a9c8299447f6dd0a8deb9012b5264acd90da
meryvl <meryvalerolaguna@gmail.com>
2023-02-21 13:13:19 +0000

    commit form validation

--- script.js
+++ script.js
@@ -5,23 +5,30 @@
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation');
-  const cvc = document.querySelector('#cvc').value;
-  const alert = document.querySelector('.alert');
+  const alert = document.querySelector('.preparedAlert');
+ 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
-        if (forms == forms.length == 0 ) {
+        if (forms == forms.length == 0 || forms === null ) {
           event.preventDefault()
           event.stopPropagation()
-          alert.innerHTML = ` <div class="alert alert-danger  fw-bold alert-none-valid" role="alert">
+          alert.innerHTML = ` <div class="alert alert-danger m-2  fw-bold alert-none-valid" role="alert">
           Some field are missing
         </div>`
         }
-        
-        
+       
         form.classList.add('was-validated')
       }, false)
+
+
     })
 })();
 
+
+
+
+
+ 
+     
